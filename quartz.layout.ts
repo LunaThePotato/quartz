0edc4d10704d01e1d00caa89e35d851fdf8bf490
LunaThePotato <lunathepotato64@gmail.com>
2025-03-13 01:58:53 -0400

    Quartz sync: Mar 13, 2025, 1:58 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'LunaThePotato/Passione-Campaign',
+        // from data-repo-id
+        repoId: 'R_kgDOOHr0Xg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOOHr0Xs4Cn-FD',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
